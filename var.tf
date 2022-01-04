a986bc513b4e0a47754523005e0e557e2a113991
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-04 09:01:50 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -18,3 +18,39 @@ variable "availability_zones" {
   type = list(string)
   default = ["us-east-2c"]
 }
+variable "name_lb" {
+  type = string
+  default = "test-lb-tf"
+}
+variable "load_balancer_type" {
+  type = string
+  default = "application"
+}
+variable "security_groups" {
+  type = list(string)
+  default = ["sg-017c097bb1674f881"]
+}
+variable " subnets" {
+  type = list(string)
+  default =  ["subnet-0ad08acb398ada09d","subnet-0a22ca2d020ca46c1"]
+}
+variable "name_TG" {
+  type = string
+  default = "tf-example-lb-tg"
+}
+variable "protocol" {
+  type = string
+  default = "HTTP"
+}
+variable "vpc_id" {
+  type = string
+  default = "vpc-048331c397b1a9bc3"
+}
+variable "protocol" {
+  type = string
+  default = "HTTP"
+}
+variable "target_id" {
+  type = string
+  default = "i-033e9c4279b6714ba"
+}
